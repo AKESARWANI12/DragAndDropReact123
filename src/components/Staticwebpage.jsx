21b490efa09150d1fccf77e6aa37e09be80f1cd0
AKESARWANI12 <95116904+AKESARWANI12@users.noreply.github.com>
2024-01-02 21:49:00 +0530

    Update Staticwebpage.jsx

--- src/components/Staticwebpage.jsx
+++ src/components/Staticwebpage.jsx
@@ -1,29 +1,4 @@
-// // StaticWebpage.jsx
-// import React, { useEffect, useState } from 'react';
 
-// const StaticWebpage = ({ items }) => {
-//   return (
-//     <div>
-//       <h1>Static Webpage</h1>
-//       <img src={photo1}></img>
-//       <div>
-//         {items.map((item, index) => (
-//           <div key={index}>
-//             {item.type === 'TEXT' ? (
-//               <p>{item.content}</p>
-//             ) : (
-//               <img src={item.content} alt={`Image ${index}`} />
-             
-//             )}
-//           </div>
-//         ))}
-//       </div>
-   
-//     </div>
-//   );
-// };
-
-// export default StaticWebpage;
 import React from 'react';
  import photo1 from "./photos/hayat 6.png";
 const Staticwebpage = ({ location }) => {
