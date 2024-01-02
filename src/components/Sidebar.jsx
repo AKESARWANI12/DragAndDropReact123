9f4ade04f42f15e94b1094cb6e68f4c8b93c29c6
AKESARWANI12 <95116904+AKESARWANI12@users.noreply.github.com>
2024-01-02 21:47:03 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,45 +1,4 @@
-// import React from 'react'
-// import Draggable from "react-draggable";
-// import photo1 from "../photo1/d3.png";
-// import photo3 from "./photos/hayat 3.png";
-// import photo4 from "./photos/hayat 4.png";
-// import photo5 from "./photos/hayat 5.png";
-// import photo6 from "./photos/hayat 6.png";
-// import "./Side.css";
-// const Sidebar = () => {
-//   return (
-//     <div className="sidecontainer">
- 
-//        <div className="textcollection">
-//        <h2>Text</h2>
-     
-//        <div className="box1"><p>OM namah shivay</p></div>
-//        <div className="box2"><p>OM namah </p></div>
-//        <div className="box3"><p>OM namah kesar</p></div>
-//        <div className="box4"><p>OM namah kriti</p></div>
-//        <div className="box5"><p>OM namah vaishu</p></div>
-       
-     
-//        </div>
 
-//        <div className="box1"><p>Toolbar</p></div>
-
-//        <div className="imagecollection">
-//        <h2>Image</h2>
-       
-//        <img src={photo1} alt="eror" height="50px" width="50px"></img>
-//        <img src={photo3} alt="eror" height="50px" width="50px"></img>
-//        <img src={photo4} alt="eror" height="50px" width="50px"></img>
-//        <img src={photo5} alt="eror" height="50px" width="50px"></img>
-//        <img src={photo6} alt="eror" height="50px" width="50px"></img>
-
-//        </div>
-   
-//  </div>
-//   )
-// }
-
-// export default Sidebar;
 
 import React from 'react';
 import Draggable from 'react-draggable';
