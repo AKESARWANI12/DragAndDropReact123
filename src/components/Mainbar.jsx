93d6f33561dfe7e3451ffe529eb3e25d81f73f4b
AKESARWANI12 <95116904+AKESARWANI12@users.noreply.github.com>
2024-01-02 21:46:20 +0530

    Update Mainbar.jsx

--- src/components/Mainbar.jsx
+++ src/components/Mainbar.jsx
@@ -1,18 +1,4 @@
-// import React from 'react'
-// import "./Main.css";
-// import Portfolio from './portfolio';
-// const Mainbar = () => {
-//   return (
-    // <div className="maincontainer">
-    //     <div className='ayush'> 
-    //            <p>Website Section</p>
-    //     </div>
-    //     <div className='kriti'>  
-    //            <button>SAVE</button> 
-    //      </div>
-     
-    // </div>
- // MainBar.jsx
+
 import React from 'react';
 import Editable from './Editable';
 
@@ -29,29 +15,4 @@ const MainBar = ({ items, onUpdateItem }) => {
 export default MainBar;
 
 
-
-    // MainBar.jsx
-// import React from 'react';
-// import Editable from './Editable';
-// import Portfolio from './portfolio';
-
-// const MainBar = ({ items, onUpdateItem }) => {
-//   return (
-//     <div>
-//       {items.map((item, index) => (
-//         <Editable key={index} item={item} onUpdate={onUpdateItem} />
-//       ))}
-    
-//       <div style={{ border: '1px solid black', padding: '10px', minHeight: '100px', minWidth: '200px' }}>
-//         {/* Render portfolio items here, you can use the same Editable component */}
-       
-//         {items.map((item, index) => (
-//           <Editable key={index} item={item} onUpdate={onUpdateItem} />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default MainBar;
 
